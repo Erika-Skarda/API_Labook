83b495171d9a4a5a34a0c5ac38076101fdff9001
Erika-Skarda <erikaskarda@yahoo.com.br>
2020-08-06 14:49:44 -0300

    deploy

--- src/data/BaseDatabase.ts
+++ src/data/BaseDatabase.ts
@@ -12,7 +12,7 @@ export abstract class BaseDatabase {
         client: "mysql",
         connection: {
           host: process.env.DB_HOST,
-          port: 3306,
+          port: Number(process.env.DB_PORT || "3306"),
           user: process.env.DB_USER,
           password: process.env.DB_PASSWORD,
           database: process.env.DB_DATABASE_NAME,
